3600b36dcd7235889794a0160f374631e0de80ae
jmoswalt <jmoswalt@gmail.com>
2012-10-13 16:08:58 -0500

    Add change_view method to admin that picks up the next querystring.

--- things/admin.py
+++ things/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
+from django.utils.encoding import iri_to_uri
 
 
 class ThingAdmin(admin.ModelAdmin):
@@ -18,3 +19,10 @@ class ThingAdmin(admin.ModelAdmin):
         )
         return link
     link.allow_tags = True
+
+    def change_view(self, request, object_id, form_url='', extra_context=None):
+        result = super(ThingAdmin, self).change_view(
+            request, object_id, form_url=form_url, extra_context=extra_context)
+        if not '_addanother' in request.POST and not '_continue' in request.POST and 'next' in request.GET:
+            result['Location'] = iri_to_uri("%s") % request.GET.get('next')
+        return result
