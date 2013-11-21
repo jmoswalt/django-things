171bf3210ae14603db0291555abd5f30b9789bbd
jmoswalt <jmoswalt@gmail.com>
2013-11-20 23:12:56 -0600

    Add exporting from admin.

--- things/urls.py
+++ things/urls.py
@@ -5,7 +5,7 @@ from django.views.decorators.cache import cache_page
 from django.views.generic import TemplateView
 
 from .pages.models import Page
-from .views import ThingDetailView, static_build
+from .views import ThingDetailView, static_build, thing_export
 from .feeds import AllThingsFeed, ThingSitemap
 
 admin.autodiscover()
@@ -21,6 +21,7 @@ urlpatterns = patterns(
         name='deploy'),
     url(r'^feed/$', AllThingsFeed(), name="feed_all"),
     url(r'^sitemap\.xml$', cache_page(3600)(sitemaps_views.sitemap), {'sitemaps': {'things': ThingSitemap}}),
+    url(r'^export/(?P<ct_id>\d+)/$', thing_export, name='things_export'),
     url(r'^(?P<slug>[\w\-\/]+)/$',
         ThingDetailView.as_view(model=Page),
         name='page_detail'),
