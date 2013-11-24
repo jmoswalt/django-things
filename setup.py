8bef21d5ad8d6707038a1b91fb86266c6fe27505
jmoswalt <jmoswalt@gmail.com>
2013-11-24 15:59:41 -0600

    Move version out of init

--- setup.py
+++ setup.py
@@ -17,9 +17,19 @@
 #    along with Django-things.  If not, see <http://gnu.org/licenses/>.
 from setuptools import setup, find_packages
 
+VERSION = (0, 4, 0)
+
+def get_version():
+    version = "%s.%s" % (VERSION[0], VERSION[1])
+    if VERSION[2] != 0:
+        version = "%s.%s" % (version, VERSION[2])
+    return version
+
+__version__ = get_version()
+
 setup(
     name='django-things',
-    version=__import__('things').__version__,
+    version=__version__,
     license='GNU Lesser General Public License (LGPL), Version 3',
     url='https://github.com/jmoswalt/django-things',
     author='John-Michael Oswalt (JMO)',
--- things/__init__.py
+++ things/__init__.py
@@ -1,13 +1,2 @@
-VERSION = (0, 4, 0)
-
-
-def get_version():
-    version = "%s.%s" % (VERSION[0], VERSION[1])
-    if VERSION[2] != 0:
-        version = "%s.%s" % (version, VERSION[2])
-    return version
-
-__version__ = get_version()
-
 from .utils import load_models
 load_models()
